6a052a8d44df8de175a1570947da604123417cea
Tlangelani Mboweni <mbowenit@gmail.com>
2017-04-07 16:08:28 +0200

    add props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import React from 'react';
 
-const App = () => <h1>React App</h1>;
+class App extends React.Component {
+    render() {
+        return <h1>{this.props.name}</h1>;
+    }
+}
+
+// expected prop types
+App.propTypes = {
+    name: React.PropTypes.string,
+    users: React.PropTypes.number.isRequired
+};
+
+// default prop values
+App.defaultProps = {
+    name: 'Jane Doe Inc.'
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <App />,
+  <App users={500} name="Spree" />,
   document.getElementById('root')
 );
